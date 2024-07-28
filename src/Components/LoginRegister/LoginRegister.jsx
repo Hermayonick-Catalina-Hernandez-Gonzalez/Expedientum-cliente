48ba862f39c5300f8ddc4fc6d6aa7d5e80e067f0
Hermayonick-Catalina-Hernandez-Gonzalez <2130036@upv.edu.mx>
2024-07-27 18:43:46 -0600

    Login y contraseña

--- src/Components/LoginRegister/LoginRegister.jsx
+++ src/Components/LoginRegister/LoginRegister.jsx
@@ -4,7 +4,6 @@ import { FaUser, FaLock } from "react-icons/fa";
 import fondoImage from '../assets/imagen.jpg';
 
 const LoginRegister = () => {
-    const [isRegister, setIsRegister] = useState(false);
     const [isForgotPassword, setIsForgotPassword] = useState(false);
 
     return (
@@ -21,67 +20,26 @@ const LoginRegister = () => {
                     <p><a href="# " onClick={() => setIsForgotPassword(false)}>Volver al inicio de sesión</a></p>
                 </form>
             ) : (
-                <form className="formulario-login" onSubmit={(e) => { e.preventDefault(); /* Lógica para manejar el inicio de sesión/registro */ }}>
-                    {isRegister ? (
-                        <>
-                            <h2>Registrarse</h2>
-                            <div className="caja-input">
-                                <FaUser className="icono" />
-                                <input type="text" placeholder="Nombre" required />
-                                <label>Nombre</label>
-                            </div>
-                            <div className="caja-input">
-                                <FaUser className="icono" />
-                                <input type="text" placeholder="Apellido" required />
-                                <label>Apellido</label>
-                            </div>
-                            <div className="caja-input">
-                                <FaUser className="icono" />
-                                <input type="text" placeholder="Usuario" required />
-                                <label>Usuario</label>
-                            </div>
-                            <div className="caja-input">
-                                <FaUser className="icono" />
-                                <input type="email" placeholder="Correo electrónico" required />
-                                <label>Correo electrónico</label>
-                            </div>
-                            <div className="caja-input">
-                                <FaLock className="icono" />
-                                <input type="password" placeholder="Contraseña" required />
-                                <label>Contraseña</label>
-                            </div>
-                            <div className="caja-input">
-                                <FaLock className="icono" />
-                                <input type="password" placeholder="Confirmar contraseña" required />
-                                <label>Confirmar contraseña</label>
-                            </div>
-                            <button type="submit">Registrar</button>
-                            <p>¿Ya tienes cuenta? <a href="# " onClick={() => setIsRegister(false)}>Iniciar sesión</a></p>
-                        </>
-                    ) : (
-                        <>
-                            <h2>Iniciar sesión</h2>
-                            <div className="caja-input">
-                                <FaUser className="icono" />
-                                <input type="text" placeholder="Usuario" required />
-                                <label>Usuario</label>
-                            </div>
-                            <div className="caja-input">
-                                <FaLock className="icono" />
-                                <input type="password" placeholder="Contraseña" required />
-                                <label>Contraseña</label>
-                            </div>
-                            <div className="opciones-login">
-                                <div className="recordar">
-                                    <input type="checkbox" id="recordar" />
-                                    <label htmlFor="recordar">Recordar</label>
-                                </div>
-                                <a href="# " onClick={() => setIsForgotPassword(true)} className="olvido-contraseña">¿Olvidaste tu Contraseña?</a>
-                            </div>
-                            <button type="submit">Iniciar</button>
-                            <p>¿Aún no tienes cuenta? <a href="# " onClick={() => setIsRegister(true)}>Registrar</a></p>
-                        </>
-                    )}
+                <form className="formulario-login" onSubmit={(e) => { e.preventDefault(); /* Lógica para manejar el inicio de sesión */ }}>
+                    <h2>Iniciar sesión</h2>
+                    <div className="caja-input">
+                        <FaUser className="icono" />
+                        <input type="text" placeholder="Usuario" required />
+                        <label>Usuario</label>
+                    </div>
+                    <div className="caja-input">
+                        <FaLock className="icono" />
+                        <input type="password" placeholder="Contraseña" required />
+                        <label>Contraseña</label>
+                    </div>
+                    <div className="opciones-login">
+                        <div className="recordar">
+                            <input type="checkbox" id="recordar" />
+                            <label htmlFor="recordar">Recordar</label>
+                        </div>
+                        <a href="# " onClick={() => setIsForgotPassword(true)} className="olvido-contraseña">¿Olvidaste tu Contraseña?</a>
+                    </div>
+                    <button type="submit">Iniciar</button>
                 </form>
             )}
             <div className="imagen-login">
